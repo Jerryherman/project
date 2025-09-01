37b1afbfbe6daf48a2bcde4750b58ab1546f4f69
Jerryherman <150015328+Jerryherman@users.noreply.github.com>
2025-09-01 19:25:44 +0530

    Delete function.js directory

--- function.js/Vowel.js
+++
@@ -1,8 +0,0 @@
-function countVowel(str){
-    const count = str.match(/[aeiou]/gi).length;
-    return count;
-}
-
-let string = "Enter a string"; 
-let result = countVowel(string);
-console.log(result);
--- function.js/task3.js
+++
@@ -1,6 +0,0 @@
-function alphabetical(string){
-    return string.split("").sort().join("")
-}
-let input = "friendship"
-console.log(input)
-console.log(alphabetical(input))
